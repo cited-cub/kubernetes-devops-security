68fa85d8cb56c9af3b69190357746063841e9eb6
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2024-01-03 22:41:15 +0100

    Deploy whole application, add volume

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -6,11 +6,13 @@ imageName="${REGISTRY_URI}/numeric-app:${GIT_COMMIT}"
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
-if [[ $? -ne 0 ]]; then
-    echo "deployment ${deploymentName} doesn't exist"
-    kubectl -n default apply -f k8s_deployment_service.yaml
-else
-    echo "deployment ${deploymentName} exists"
-    echo "image name - ${imageName}"
-    kubectl -n default set image deploy ${deploymentName}=${imageName} --record=true
-fi
+# if [[ $? -ne 0 ]]; then
+#     echo "deployment ${deploymentName} doesn't exist"
+#     kubectl -n default apply -f k8s_deployment_service.yaml
+# else
+#     echo "deployment ${deploymentName} exists"
+#     echo "image name - ${imageName}"
+#     kubectl -n default set image deploy ${deploymentName}=${imageName} --record=true
+# fi
+
+kubectl -n default apply -f k8s_deployment_service.yaml
