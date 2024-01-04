5c8812cb26b284dcfbf397f3ce21b654eef1c6a5
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2024-01-05 00:33:37 +0100

    Fix X-Content-Type-Options header error

--- zap.sh
+++ zap.sh
@@ -3,10 +3,7 @@
 # first run this
 # chmod 777 $(pwd)
 echo $(id -u):$(id -g)
-pwd
 ln -s $(pwd) /zap/wrk
-ls /zap/
-ls /zap/wrk
 zap-api-scan.py -t http://${applicationURL}:${PORT}/v3/api-docs -f openapi -r zap_report.html
 
 exit_code=$?
