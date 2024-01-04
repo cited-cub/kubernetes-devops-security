70ac47cf228fec1bb96c043ad3b639c69c68a117
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2024-01-04 23:33:18 +0100

    Update zap.sh

--- zap.sh
+++ zap.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# PORT=$(kubectl -n default get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
-
-# # first run this
+# first run this
 # chmod 777 $(pwd)
-# echo $(id -u):$(id -g)
-# zap-api-scan.py -t 
-echo $PORT
-echo $applicationURL:$PORT$applicationURI
+echo $(id -u):$(id -g)
+zap-api-scan.py -t ${applicationURL}:${PORT}/v3/api-docs -f openapi -r zap_report.html
+
+exit_code=$?
+
+# # HTML report
+# sudo mkdir -p owasp-zap-report
+# sudo mv zap_report.html owasp-zap-report
+
+echo "Exit code: $exit_code"
+
+if [[ ${exit_code} -ne 0 ]]; then
+    echo "OWASP ZAP Report has either Low/Medium/High risk. Please check the HTML report"
+    exit 1;
+else
+    echo "OWASP ZAP did not report any risk"
+fi;
