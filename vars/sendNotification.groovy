f41deccf8b677e67b9b93b1531bd114d4d0f2119
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2024-01-05 20:37:08 +0100

    Test

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -13,5 +13,5 @@ def call(String buildStatus = 'STARTED') {
 
     def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
 
-    slackSend(color: color, message: msg)
+    slackSend(color: color, message: msg, channel: "jenkins")
 }
