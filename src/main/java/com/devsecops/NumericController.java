e2d1a8efaae3efe7857776096ff41e8666099449
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2024-01-22 09:25:38 +0100

    Update NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -27,7 +27,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps II";
+			return "Kubernetes DevSecOps";
 		}
 
 		@GetMapping("/compare/{value}")
