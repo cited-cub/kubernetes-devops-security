92a11fab0ed75e558e58c7877000cc658bfef468
Andreas Erlandsson <andreas.erlandsson@gmail.com>
2026-05-17 14:58:06 +0000

    Bump

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -27,7 +27,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps v4.0";
+			return "Kubernetes DevSecOps v5.0";
 		}
 
 		@GetMapping("/compare/{value}")
